ccd6674db1cb06f6a36238b39e561f7e110a3bf9
Aharshi Bhattacharjee <aharshibhattacharjee@Ritayans-MacBook-Air.local>
2026-02-14 11:21:59 -0500

    Add document routes to main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const patientRoutes = require('./patientRoutes');
 const batchRoutes = require('./batchRoutes');
 const visitRoutes = require('./visitRoutes');
 const journalRoutes = require('./journalRoutes');
+const documentRoutes = require('./documentRoutes');
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/patients', patientRoutes);
 router.use('/batches', batchRoutes);
 router.use('/', visitRoutes);
 router.use('/', journalRoutes);
+router.use('/', documentRoutes);
 
 module.exports = router;
